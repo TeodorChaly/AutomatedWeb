8424727a90588a17eab7bcd2df54293cb97fc241
Theodor <teodors.calijs@gmail.com>
2022-05-28 15:24:44 +0300

    Created AutomatedWeb(Selenium)

---
+++ Selenium.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+PATH = "C:\Go\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("http://www.google.com")
+def accept_cookies():
+    accept_button = driver.find_element(By.ID, 'L2AGLb')
+    accept_button.click()
+def search():
+    search_box = driver.find_element(By.NAME, "q")
+    search_box.send_keys("webdriver")
+
+accept_cookies()
+search()
+
+
+
+time.sleep(10)
+
+driver.close()
+driver.quit()
